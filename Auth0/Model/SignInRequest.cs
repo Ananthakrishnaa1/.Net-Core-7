a8ad4d48677a9b68d78ed00d862cc084605e4a4d
Ananthakrishnaa1 <ananthkrishnaa1@gmail.com>
2023-03-16 15:47:31 +0530

    Auth0 SignIn

--- Auth0/Model/SignInRequest.cs
+++ Auth0/Model/SignInRequest.cs
@@ -1,9 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace Main.Service.Auth0.Model
 {
     public class SignInRequest
     {
+        [JsonProperty("username")]
+        public string? Username { get; set; }
+
+        [JsonProperty("password")]
+        public string? Password { get; set; }
+
+        [JsonProperty("audience")]
+        public string? Audience { get; set; }
+
+        [JsonProperty("scope")]
+        public string? Scope { get; set; }
+
+        [JsonProperty("client_id")]
+        public string? ClientId { get; set; }
+
+        [JsonProperty("client_secret")]
+        public string? ClientSecretId { get; set; }
+
+        [JsonProperty("grant_type")]
+        public string? GrantType { get; set; }
+
+        [JsonProperty("realm")]
+        public string? Realm { get; set; }
 
     }
 }
--- Auth0/Model/SignInResponse.cs
+++ Auth0/Model/SignInResponse.cs
@@ -1,6 +1,22 @@
-﻿namespace Main.Service.Auth0.Model
+﻿using Newtonsoft.Json;
+
+namespace Main.Service.Auth0.Model
 {
     public class SignInResponse
     {
+        [JsonProperty("access_token")]
+        public string? AccessToken { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string? RefereshToken { get; set; }
+
+        [JsonProperty("id_token")]
+        public string? IdToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string? TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
     }
 }
