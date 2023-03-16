b6480fa61ce85e3fa8128376a55ef6a198468905
Ananthakrishnaa1 <ananthkrishnaa1@gmail.com>
2023-03-16 15:47:31 +0530

    changes

---
+++ System/Linq/SystemCore_EnumerableDebugView.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace System.Linq
+{
+    internal class FetchResult<T>
+    {
+        public JToken? result;
+
+        public FetchResult(JToken? result)
+        {
+            this.result = result;
+        }
+    }
+}
